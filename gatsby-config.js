ca82381ceac6fb4293638e27c585733b201d23d9
Gabriel J. Angelini-Knoll <gabe.angelini.knoll@gmail.com>
2020-08-16 09:46:48 +0200

    updated config and json

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,8 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    module.exports = {
+      pathPrefix: "/Website2020",
+    }
   ],
 }
