334aa3b03ec92e9f8af5735b48a6794e8cc471ec
Gabriel J. Angelini-Knoll <gabe.angelini.knoll@gmail.com>
2020-10-04 10:46:14 +0200

    fixed picture

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "me.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "gabe.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
