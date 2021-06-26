4b0d6936cd52eefc2a0d1413474d4406f8db3fd6
Gabriel J. Angelini-Knoll <gabe.angelini.knoll@gmail.com>
2021-06-26 09:11:30 +0200

    june26

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,9 @@ const IndexPage = () => (
     <div style={{ float: `left`, maxWidth: `70%`, marginBottom: `1.45rem`}}>
     <p>I am a Postdoctoral Researcher at the <a href="https://www.mi.fu-berlin.de/math/groups/top/members/index.html">Freie Universit<span>&#228;</span>t Berlin</a>. Previously, I was a Postdoctoral Researcher at <a href="https://math.msu.edu">Michigan State University</a>. 
       I earned my PhD at <a href="https://clas.wayne.edu/math">Wayne State University</a> under the direction of <a href="https://clasprofiles.wayne.edu/profile/dy8211">Andrew Salch</a>.</p>
-    <p>Broadly, I study interactions between algebraic topology, arithmetic and geometry. Specifically, I am interested invariants of ring spectra, such as topological Hochschild homology, algebraic K-theory, and topological cyclic homology.</p>
+    <p>Broadly, I study interactions between algebraic topology, arithmetic and geometry. Specifically, I am interested invariants of ring spectra, such as algebraic K-theory and factorization homology, and interactions with chromatic homotopy theory, equivariant homotopy theory, and motivic homotopy theory.</p>
 		<p>In addition to my research activity, I have had the opportunity to teach a broad range of courses, 
-      mentor undergraduate research projects, and organize several conferences and seminars. For a more detailed description of my activities see my <CV />.</p>
+      mentor research projects, and organize several conferences and seminars. For a more detailed description of my activities see my <CV />.</p>
     </div>
     <div style={{marginLeft: `70%`, maxWidth: `30%`, marginBottom: `1.45rem` }} > 
      <Image />
